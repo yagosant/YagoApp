2cb1b483b14e8091461815e1f420022173b6e12f
Yago Santos <yagojf27@gmail.com>
2020-04-30 17:41:53 -0300

    start App

--- app/src/main/java/com/yago/yagoapp/Principal.java
+++ app/src/main/java/com/yago/yagoapp/Principal.java
@@ -2,7 +2,10 @@ package com.yago.yagoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Principal extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Button btnSobre = findViewById(R.id.btnSobre);
+
+        btnSobre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Principal.this, Sobre.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/yago/yagoapp/Sobre.java
@@ -0,0 +1,14 @@
+package com.yago.yagoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
